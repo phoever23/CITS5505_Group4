cbb13d9b59ccff98ba20094398eaeb244ce32042
phoever23 <gaoming93@foxmail.com>
2025-05-07 20:43:41 +0800

    Delete seed.py file because now we don't need python to generate fake data

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for, request, jsonify, redirect, f
 from config import Config
 from models import db, User, Expense
 from flask_migrate import Migrate
-import random
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 import io
@@ -217,44 +216,6 @@ def api_expenses():
             'currency': exp.currency
         })
     return jsonify(result)
-
-
-# Optional: Simple route to seed dummy data directly from browser
-@app.route('/seed')
-def seed_data():
-    user = User.query.filter_by(username='demo_user').first()
-    if not user:
-        user = User(username='demo_user', password_hash='dummyhash')
-        dummy_user = User(id='991', username='Niranjan', password_hash='sdlkjf439053kjn')
-        db.session.add(user)
-        db.session.add(dummy_user)
-        db.session.commit()
-
-    categories = {
-        "Housing": ["rent", "mortgage"],
-        "Food": ["grocery", "restaurants"],
-        "Shopping": ["clothes", "electronics"],
-        "Education": ["tuition", "printing"],
-        "Others": ["gifts", "transport", "maintenance"]
-    }
-
-    currencies = ["AUD", "GBP", "USD", "CAD", "EUR"]
-
-    for _ in range(50):
-        cat = random.choice(list(categories.keys()))
-        sub = random.choice(categories[cat])
-        exp = Expense(
-            date=datetime.strptime(f"2024-{random.randint(1, 12):02d}-{random.randint(1, 28):02d}", '%Y-%m-%d'),
-            category=cat,
-            sub_category=sub,
-            amount=round(random.uniform(10, 500), 2),
-            currency=random.choice(currencies),
-            author=user
-        )
-        db.session.add(exp)
-
-    db.session.commit()
-    return 'Dummy data inserted! Go to /dashboard now.'
 
 if __name__ == '__main__':
     app.run(debug=True)
